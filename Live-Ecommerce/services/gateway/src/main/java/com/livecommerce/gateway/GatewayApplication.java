0606c5fa0e3c0ee6c5e7cc2a8802582e522a85aa
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-10-27 18:26:27 +0530

    Live App

--- Live-Ecommerce/services/gateway/src/main/java/com/livecommerce/gateway/GatewayApplication.java
+++ Live-Ecommerce/services/gateway/src/main/java/com/livecommerce/gateway/GatewayApplication.java
@@ -1,6 +1,7 @@
 package com.livecommerce.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class GatewayApplication {
